fb76cbc296b1fe55c435fb45c69c68640a379338
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-05-04 12:23:16 +0600

    add useNavigate

--- src/Components/Posts/Post/PostDetails/PostDetails.jsx
+++ src/Components/Posts/Post/PostDetails/PostDetails.jsx
@@ -1,13 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const postDetails = useLoaderData();
     const{title, body}= postDetails;
+    const navigate = useNavigate();
+    const handleGoBack =() =>{
+
+        navigate(-1);
+    }
     return (
         <div>
             <h2>Post details of: {title}</h2>
             <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
             
         </div>
     );
